8222e5369e74d66ff6f4749dc38a1722f2ae13a0
RAVIIPPILI <32967256+RAVIIPPILI@users.noreply.github.com>
2024-02-25 16:58:42 -0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,3 +1,4 @@
 import streamlit as st
 from streamlit_player import st_player
-st_player("https://www.youtube.com/watch?v=byUbhCt952U")
+#st_player("https://www.youtube.com/watch?v=byUbhCt952U")
+st_player("https://watchkin.com/53bc98f90f")
